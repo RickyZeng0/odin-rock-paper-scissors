50b7bb921e0528388b559837e54970089d0105ec
Ricky_Zeng <zengchiwai08@gmail.com>
2025-07-02 15:34:39 +0800

    write the logic to get the computer choice

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log("Testing")
+function getComputerChoice(){
+    let randNum = Math.random();
+    if(randNum <= 0.3) return "rock";
+    else if(randNum <= 0.6) return "paper";
+    else return "scissors";
+}
